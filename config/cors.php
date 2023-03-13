1ac7d75aa013e5abf0cb8ffcc0d2a451b6b56904
Marco 9726 <marcosarra9@gmail.com>
2023-03-13 17:52:03 +0100

    Settaggio CORS policy

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
 
 return [
 
-    /*
+	/*
     |--------------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
     |--------------------------------------------------------------------------
@@ -15,20 +15,20 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+	'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+	'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+	'allowed_origins' => [env('APP_FRONTEND_URL', "http://127.0.0.1:5173")],
 
-    'allowed_origins_patterns' => [],
+	'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+	'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+	'exposed_headers' => [],
 
-    'max_age' => 0,
+	'max_age' => 0,
 
-    'supports_credentials' => false,
+	'supports_credentials' => false,
 
 ];
