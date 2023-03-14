43e5caaf1707f2439da7cff16625c313f262d3d7
Marco 9726 <marcosarra9@gmail.com>
2023-03-14 17:57:28 +0100

    metodo show con annessa rotta in api.php

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -18,4 +18,21 @@ class ProjectController extends Controller
 			'results' => $projects
 		]);
 	}
+
+	public function show($slug)
+	{
+		$project = Project::with('type', 'technologies')->where('slug', $slug)->first();
+
+		if ($project) {
+			return response()->json([
+				'success'  => true,
+				'result'  => $project
+			]);
+		} else {
+			return response()->json()([
+				'success' => false,
+				'error' => 'Nessun progetto trovato'
+			]);
+		}
+	}
 }
