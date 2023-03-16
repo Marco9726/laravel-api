6a44a13f5ed9de69fcc9d9e197c49319836f12bf
Marco 9726 <marcosarra9@gmail.com>
2023-03-16 15:42:09 +0100

    creazione view guest_email

---
+++ resources/views/emails/guest_email.blade.php
@@ -0,0 +1,8 @@
+<h1>Nuovo contatto da Boolpress</h1>
+<p>
+	Nome: {{$lead->name}} <br>
+	Cognome: {{ $lead->surname }} <br>
+	Email: {{ $lead->mail }} <br>
+	Telefono: {{ $lead->phone }} <br>
+	Messaggio: {{ $lead->message }}
+</p>
--- resources/views/emails/new_post_added.blade.php
+++ resources/views/emails/new_post_added.blade.php
@@ -1,7 +1,7 @@
 <h1>Nuovo progetto inserito</h1>
 <p>
 	Nuovo progetto inserito <br>
-	Titolo: {{ $lead->title }}
-	Slug: {{ $lead->slug }}
+	Titolo: {{ $lead->title }} <br>
+	Slug: {{ $lead->slug }} <br>
 	Descrizione: {{ $lead->description }}
 </p>
